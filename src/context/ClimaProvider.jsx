31fd04737a4e63c5d42a277fd25f2f92a3822bfa
Claudio Martin Villa Sono <mvillasono@gmail.com>
2023-04-19 22:57:00 -0500

    Cambiando a https la peticion al api de open wheater

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -20,7 +20,7 @@ const ClimaProvider = ({ children }) => {
     try {
       const { ciudad, pais } = datos;
       const appId = import.meta.env.VITE_API_KEY;
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},PE&limit=1&appid=${appId}`;
+      const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},PE&limit=1&appid=${appId}`;
       const { data } = await axios(url);
       const { lat, lon } = data[0];
       const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
